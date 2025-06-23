2c864cf0ae985dc274da904ce920103bef75cedf
SERMAD AHMAD <sermadahmad024@gmail.com>
2025-06-23 15:35:48 +0500

    feat: splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import BootSplash from "react-native-bootsplash";
 
 const App = () => {
+    useEffect(() => {
+        const init = async () => {
+            // …do multiple sync or async tasks
+        };
+
+        init().finally(async () => {
+            await BootSplash.hide({ fade: true });
+            console.log("BootSplash has been hidden successfully");
+        });
+    }, []);
+
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Hello, World!</Text>
